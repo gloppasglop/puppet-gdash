0d82860dcab432bf4ffc9596cd409d3d419637ab
Bill Fraser <fraser@pythian.com>
2013-12-20 23:53:15 -0500

    Fix class and parameter descriptions

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,16 +1,10 @@
-# Class: apache::params
+# Class: gdash::params
 #
-# This class manages Apache parameters
+# This class manages GDash parameters
 #
 # Parameters:
-# - The $user that Apache runs as
-# - The $group that Apache runs as
-# - The $apache_name is the name of the package and service on the relevant
-#   distribution
-# - The $php_package is the name of the package that provided PHP
-# - The $ssl_package is the name of the Apache SSL package
-# - The $apache_dev is the name of the Apache development libraries package
-# - The $conf_contents is the contents of the Apache configuration file
+# - The $graphite_host for GDash to pull data from
+# - The $gdash_root is the directory GDash is installed to
 #
 # Actions:
 #
