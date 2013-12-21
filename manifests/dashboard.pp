392fbf0c244d492a8b81ef696c12611b80683bdc
Bill Fraser <fraser@pythian.com>
2013-12-20 21:44:20 -0500

    Applying fixes to comply with style guide (puppet-lint)

--- manifests/dashboard.pp
+++ manifests/dashboard.pp
@@ -1,25 +1,26 @@
+# Create a dashboard
 define gdash::dashboard(
-    $category = "servers",
-    $description = "",
+    $category = 'servers',
+    $description = '',
     $graph_properties = {}
 ) {
     Gdash::Category[$category] -> Gdash::Dashboard[$title]
 
     $dashboard_dir = "${gdash::configure::template_dir}/${category}/${title}"
 
-    file { "$dashboard_dir":
+    file { $dashboard_dir:
         ensure  => directory,
-        owner   => "root",
-        group   => "root",
-        mode    => 0755,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
     }
 
     file { "${dashboard_dir}/dash.yaml":
         ensure  => file,
-        owner   => "root",
-        group   => "root",
-        mode    => 0644,
-        content => template( "gdash/dash.yaml.erb" ),
-        require => File["$dashboard_dir"],
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => template( 'gdash/dash.yaml.erb' ),
+        require => File[$dashboard_dir],
     }
 }
