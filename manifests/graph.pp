f1dd7991c9c7a39a6e0e6fa56fb8970b5a7bf3ef
Bill Fraser <fraser@pythian.com>
2013-12-20 21:48:00 -0500

    Applying fixes to comply with style guide (puppet-lint)

--- manifests/graph.pp
+++ manifests/graph.pp
@@ -1,9 +1,10 @@
+# Create a graph
 define gdash::graph(
     $graph_title = $title,
-    $vtitle = "",
-    $description = "",
-    $area = "none",
-    $category = "servers",
+    $vtitle = '',
+    $description = '',
+    $area = 'none',
+    $category = 'servers',
     $dashboard = $hostname,
     $ymin = undef,
     $ymax = undef
@@ -13,12 +14,12 @@ define gdash::graph(
     $graph_file = "${gdash::configure::template_dir}/${category}/${dashboard}/${title}.graph"
 
     datacat { $graph_file:
-        template    => "gdash/graph.erb",
+        template    => 'gdash/graph.erb',
     }
 
     datacat_fragment { "${category}_${title}":
         target      => $graph_file,
-        loglevel    => "debug",
+        loglevel    => 'debug',
         data        => {
             area        => $area,
             ymax        => $ymax,
