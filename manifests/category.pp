06e820daf1b573e75d3b6437cb5ae137d55401d4
Bill Fraser <fraser@pythian.com>
2013-12-20 21:55:45 -0500

    Applying fixes to comply with style guide (puppet-lint)

--- manifests/category.pp
+++ manifests/category.pp
@@ -1,10 +1,11 @@
+# Create a directory to contain your dashboard
 define gdash::category() {
-    Class["gdash::configure"] -> Gdash::Category["$title"]
+    Class['gdash::configure'] -> Gdash::Category[$title]
 
     file { "${gdash::configure::template_dir}/${title}":
         ensure  => directory,
-        owner   => "root",
-        group   => "root",
-        mode    => 0755,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
     }
 }
